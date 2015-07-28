5189aa0009450f614a5c3545ea2b7b50eff80fa3
yusong1 <bjslxxx@126.com>
2015-07-28 17:08:42 -0400

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  respond_to :json, :xml
+  def create
+  	user = User.authenticate(params[:email], params[:password])
+  	if user
+  		respond_with user, status: :created
+  	else
+  		respond_with {}, status: 500
+  	end
+  end
+
+  def destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   belongs_to :patient, foreign_key: 'patient_id'
   belongs_to :provider, foreign_key: 'provider_npi'
   validates :fname, :lname, :email_address, presence: true
+
+  def self.authenticate(email, password)
+  	where(email_address: email, password: password).first
+  end
 end
