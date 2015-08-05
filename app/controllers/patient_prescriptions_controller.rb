6ab20bb681ff1843925c8c2a805cccb71781d080
yusong1 <bjslxxx@126.com>
2015-08-05 17:43:12 -0400

    a

--- app/controllers/patient_prescriptions_controller.rb
+++ app/controllers/patient_prescriptions_controller.rb
@@ -1,15 +1,17 @@
 class PatientPrescriptionsController < ApplicationController
   before_action :set_patient
   def create
-    rx = PatientPrescription.new(rx_params)
-    item = PatientPrescriptionItem.new(item_params)
-    ActiveRecord::Base.transaction do
-      if rx.save! and item.save!
-        render status: 500
-      else
-        render status: 200
-      end
-    end
+    # rx = PatientPrescription.new(rx_params)
+    # item = PatientPrescriptionItem.new(item_params)
+    # ActiveRecord::Base.transaction do
+    #   if rx.save! and item.save!
+    #     render status: 500
+    #   else
+    #     render status: 200
+    #   end
+    # end
+    puts params
+    render json: params, status: 200
   end
 
   private
