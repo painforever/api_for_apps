42474505d889c74c188535a11d1f49da55714bd8
yusong1 <bjslxxx@126.com>
2015-08-07 14:58:28 -0400

    a

--- app/controllers/patient_prescriptions_controller.rb
+++ app/controllers/patient_prescriptions_controller.rb
@@ -16,6 +16,15 @@ class PatientPrescriptionsController < ApplicationController
     #render json: params, status: 200
   end
 
+  def show
+    @med = Medication.find(params[:id])
+    if @med
+      render json: @med, status: 200
+    else
+      render json: "", status: 500
+    end
+  end
+
   private
   def set_patient
     @patient = Patient.find_by(patient_id: params[:patient_id]) if params.has_key? :patient_id
