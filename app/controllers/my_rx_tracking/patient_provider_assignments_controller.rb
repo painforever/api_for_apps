5930281dc6410854336e50bc05287ca4a4a0021d
yusong1 <bjslxxx@126.com>
2015-09-02 17:08:45 -0400

    a

--- app/controllers/my_rx_tracking/patient_provider_assignments_controller.rb
+++ app/controllers/my_rx_tracking/patient_provider_assignments_controller.rb
@@ -1,7 +1,14 @@
 class MyRxTracking::PatientProviderAssignmentsController < ApplicationController
   def create
     ppa = PatientProviderAssignment.new(patient_provider_assignment_params)
+    ppa.date_assigned = Time.now
     render_when_save(ppa)
+  end
+
+  def index
+    patient = Patient.find(params[:patient_id])
+    npis = patient.npis
+    render json: npis, status: 200
   end
 
   def patient_provider_assignment_params
