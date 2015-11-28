a0119134b8bbebc5abda095d834951f02ebd7e2f
yusong1 <bjslxxx@126.com>
2015-11-28 10:30:31 -0500

    a

--- app/controllers/diabetes/medications_controller.rb
+++ app/controllers/diabetes/medications_controller.rb
@@ -1,3 +1,6 @@
 class Diabetes::MedicationsController < ApplicationController
-  
+  def index
+  	drugs = PatientReportedMedication.with_medications.where(patient_id: params[:patient_id])
+  	render json: drugs, status: :ok
+  end
 end
